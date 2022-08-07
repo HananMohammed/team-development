b0ff72f6bbc6ec897d975d5260466d59e812220e
hananamin111995 <102970138+hananamin111995@users.noreply.github.com>
2022-08-07 10:42:08 +0200

    Component Responsibility

--- vuejs-topics/vue3-revision/js/components/AssignmentList.js
+++ vuejs-topics/vue3-revision/js/components/AssignmentList.js
@@ -1,8 +1,10 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
     components: {
-        'assignment': Assignment
+        'assignment': Assignment,
+        'AssignmentTags':AssignmentTags
     },
     template: `
         <section v-show="assignments.length">
@@ -10,17 +12,11 @@ export default {
                 {{title}}
                 <span>({{assignments.length}})</span>
             </p>
-            <div class="flex gap-2">
-                <button 
-                v-for="tag in tags" 
-                @click="currentTag = tag"
-                class="border rounded px-1 py-px text-xs"
-                :class="{
-                    'border-blue-500 text-blue-500': tag === currentTag
-                }"
-                >{{tag}}</button>
-            </div>
-            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+            <assignment-tags 
+            @change="currentTag = $event"
+            :currentTag="currentTag"
+            :initialTags="assignments.map(a => a.tag)" ></assignment-tags>
+             <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment  v-for="assignment in filteredAssignments" :key="assignment.id" :assignment="assignment"></assignment>
             </ul>
         </section>
@@ -42,10 +38,6 @@ export default {
             }
             return this.assignments.filter(a => a.tag === this.currentTag)
         },
-        tags() {
-            //All the elements of a Set must be unique
-            return ['all', ...new Set(this.assignments.map(a => a.tag))]
-        }
     }
 
 }
