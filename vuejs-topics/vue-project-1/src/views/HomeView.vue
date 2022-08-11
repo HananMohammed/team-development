2a6555c19cba0dc5708ccc092e99ee44b0b40889
hanan m7md <hanan.amin@roqay.com>
2022-08-11 05:38:47 +0200

    Two Mental Leaps to Script Setup

--- vuejs-topics/vue-project-1/src/views/HomeView.vue
+++ vuejs-topics/vue-project-1/src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
+<script lang="ts">
 import TheWelcome from '@/components/TheWelcome.vue'
+import {onMounted, ref} from "vue";
+
+export default {
+  components: {TheWelcome},
+  setup(){
+    let message = ref("Hello World");
+
+    setTimeout(()=>{
+      message.value = "I have been changed !!"
+    }, 2000)
+    return{
+      message
+    }
+  }
+}
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <p>{{message}}</p>
+    <input type="text" v-model="message">
   </main>
 </template>
